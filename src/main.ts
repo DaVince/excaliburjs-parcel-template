4573cd323e6a5f058f7ae2992665858601ce7a6d
Vincent Beers <vincentbeers@gmail.com>
2022-02-13 10:12:31 +0100

    Update Excalibur to 25.3

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as ex from 'excalibur';
 
 const game = new ex.Engine();
-const actor = new ex.Actor(100, 100, 32, 32, ex.Color.Red);
+const actor = new ex.Actor({x: 100, y: 100, width: 32, height: 32, color: ex.Color.Red});
 game.add(actor);
 game.start();
 // If you see a red square, congratulations! It works!
