43deee793d6ad487a9576b28df839ddea22c0357
Vincent Beers <vincentbeers@gmail.com>
2022-02-13 09:28:20 +0100

    Add visual for better indication that the game works

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import * as ex from 'excalibur';
 
 const game = new ex.Engine();
+const actor = new ex.Actor(100, 100, 32, 32, ex.Color.Red);
+game.add(actor);
 game.start();
+// If you see a red square, congratulations! It works!
